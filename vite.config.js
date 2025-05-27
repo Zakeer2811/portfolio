b4a74e0b0a5da249d66999ab57dc82a7807d5997
MOHAMMAD JAKIR SHAIK <116374751+Zakeer2811@users.noreply.github.com>
2025-05-27 16:07:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
-   base: 'http://www.jakirshaik.me/portfolio/portfolio/'
-})
+  base: '/portfolio/',  // This is your intended base URL
+});
